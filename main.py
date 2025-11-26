5d6035f3b62556d55bb91fdf944bcd6c92a1866e
Jeffrey Mouritzen <jeffreymouritzen@gmail.com>
2025-11-26 05:12:42 -0500

    Step 3: Data Testing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from omegaconf import DictConfig
 _steps = [
     "download",
     "basic_cleaning",
-    #"data_check",
+    "data_check",
     #"data_split",
     #"train_random_forest",
     # NOTE: We do not include this in the steps so it is not run by mistake.
@@ -65,10 +65,18 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+        
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,12 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    # This ensures the dataset size is reasonable — not too small and not too large.
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    # This checks that all price values fall within the specified minimum and maximum bounds.
+    assert data['price'].between(min_price, max_price).all()
